d040b989252e99bc102f2005a92eccb03e23a95f
MaxenceLenoir <maxence.lenoir1206@gmail.com>
2020-12-29 15:17:13 +0100

    Football score App

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { MatchReader } from './MatchReader';
+import {Summary} from './Summary';
+
+const matchReader = MatchReader.fromCsv('football.csv');
+matchReader.load();
+
+const summary = Summary.winsAnalysisWithHtmlReport('Arsenal');
+
+summary.buildAndPrintReport(matchReader.matches)
